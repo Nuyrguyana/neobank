1fa85afd4393f98cde519418b07dde19592c4199
nurguanadeeva <enee.kors@mail.ru>
2022-11-10 17:03:19 +0300

    create header and header navigations routes

---
+++ src/containers/CreditCard/creditCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const CreditCard = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
---
+++ src/containers/Header/header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import './style.css'
+import '../../index.css'
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <div className='header-container'>
+                <span className='brand'>NeoBank</span>
+                <div className='nav'>
+                    <Link className='header-link' to='/creditCard'>Credit cart</Link>
+                    <Link className='header-link' to='/product'>Product</Link>
+                    <Link className='header-link' to='account'>Account</Link>
+                    <Link className='header-link' to='/resources'>Resources</Link>
+                </div>
+                <button className='header-btn'>Online Bank</button>
+            </div>
+        </header>
+    );
+};
+
